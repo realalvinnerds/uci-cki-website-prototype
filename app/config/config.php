ffbe4b288050017373bb82e6793ede27c7a9f90c
Alvin Nguyen <realalvinnerds@gmail.com>
2019-02-20 21:05:10 -0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://immense-brushlands-25104.herokuapp.com/';
+$config['base_url'] = 'https://uci-cki-website-prototype.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
